99b0572861059c12799385b3f78f826e9c6d1e41
Nick <ntoukas.nick@gmail.com>
2022-11-13 12:37:46 -0500

    logic complete, project complete

---
+++ app.js
@@ -0,0 +1,21 @@
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+// 1st way
+// toggleBtn.addEventListener('click', () => {
+//   if (sidebar.classList.contains('show-sidebar')) {
+//     sidebar.classList.remove('show-sidebar');
+//   } else {
+//     sidebar.classList.add('show-sidebar');
+//   }
+// });
+
+// Best way
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('show-sidebar');
+});
+
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+});
